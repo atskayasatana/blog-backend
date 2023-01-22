50b438178483cb780d9c20aa976383df033d3331
atskayasatana <88509622+atskayasatana@users.noreply.github.com>
2023-01-22 23:42:01 +0300

    added static settings

--- sensive_blog/urls.py
+++ sensive_blog/urls.py
@@ -2,6 +2,9 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf.urls.static import static
 from django.conf import settings
+from django.conf.urls import url
+
+from django.contrib.staticfiles.urls import staticfiles_urlpatterns
 
 from blog import views
 
@@ -14,9 +17,11 @@ urlpatterns = [
 ]
 
 urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
 
 if settings.DEBUG:
     import debug_toolbar
     urlpatterns += [
         path('__debug__/', include(debug_toolbar.urls)),
     ]
+
